616d0038b1fe2113fca0a7cfb6b5acc776f5fcb5
rbell594@gmail.com <rbell594@gmail.com>
2023-10-14 13:20:25 -0700

    init

---
+++ journy/journyApp.swift
@@ -0,0 +1,17 @@
+//
+//  journyApp.swift
+//  journy
+//
+//  Created by Rodrigo Bell on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct journyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
